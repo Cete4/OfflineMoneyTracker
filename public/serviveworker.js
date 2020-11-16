29ed0c5e1582309c7e17a7c206f5e05fa9369486
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-16 17:11:15 -0600

    Added activate utility

--- public/serviveworker.js
+++ public/serviveworker.js
@@ -57,3 +57,21 @@ self.addEventListener("fetch", function (evt) {
         })
     );
 });
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
